29aec3944b0147176014943542489b97988cb139
lxt <luxintong1988@gmail.com>
2017-10-30 23:17:59 +0800

    Add AdminUserView to combine two views

---
+++ src/com/ui/user/mainapp/AdminUserView.java
@@ -0,0 +1,44 @@
+package com.ui.user.mainapp;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+import com.ui.admin.UIFrame;
+import com.ui.user.mainframe.UserMainFrame;
+
+public class AdminUserView {
+
+	public static void main(String args[])
+	{
+		java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+		String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+			UIFrame frame = new UIFrame();
+			new UserMainFrame().setVisible(true);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+            }
+	});
+	}
+}
